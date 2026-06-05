506f8c8f787a4d21a78b008f3cc25492b44be6dd
Chase Owens <chasejonathanowens@gmail.com>
2026-06-04 22:41:51 -0500

    Add heading to Contact module

--- src/lib/components/modules/ContactModule.svelte
+++ src/lib/components/modules/ContactModule.svelte
@@ -2,6 +2,13 @@
 	import ContactForm from '../ui/ContactForm.svelte';
 </script>
 
-<section>
+<section class="space-y-8">
+	<div>
+		<h2 class="mt-2 text-2xl sm:text-3xl">Furniture Concierge</h2>
+		<p class="text-muted mt-4 text-lg">
+			Tell us about your space, style, and budget. We'll help find the right piece or source
+			something similar.
+		</p>
+	</div>
 	<ContactForm />
 </section>
